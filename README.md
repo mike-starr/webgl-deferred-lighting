b95bf632bb33605a7075687d2f6206ddb96d141c
Mike Starr <mstarr@gmail.com>
2018-10-27 14:14:31 -0700

    webpack-dev-server setup.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # webgl-experiments
-
-brew install node
-
-npm install -g webpack webpack-cli
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
+    devServer: {
+        contentBase: './dist'
+    },
+
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
@@ -27,8 +31,8 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
+    /*externals: {
         "react": "React",
         "react-dom": "ReactDOM"
-    }
+    }*/
 };
