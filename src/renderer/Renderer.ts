90464b1ba81fb2a365b82fcf5e7ff553fb23f712
Mike Starr <mstarr@gmail.com>
2018-10-29 20:20:12 -0700

    Fix WebGL2 types, clear buffer on render.

--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -1,17 +1,21 @@
 export default class Renderer {
 
-    private glContext: WebGLRenderingContext | null;
+    private gl: WebGL2RenderingContext | null;
 
     constructor(private readonly element: HTMLCanvasElement) {
-        this.glContext = element.getContext("experimental-webgl");
+        this.gl = element.getContext('webgl2');
+
+        if (!this.gl) {
+            throw new Error("Failed to initialize webgl context.");
+        }
     }
 
     render() {
+        if (!this.gl) {
+            return;
+        }
 
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
     }
-
-
-
-
-
 }
